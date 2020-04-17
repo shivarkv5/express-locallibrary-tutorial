c26bfccb15e10a842434562ad183b0e8033222fc
srkvodnala <srkrao547@gmail.com>
2020-04-17 14:27:57 -0500

    Controllers

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const dotenv = require('dotenv');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
 
 const app = express();
+dotenv.config({ path: '.env' });
+
 
 //mongodb
 const mongoose = require('mongoose');
-const dev_db_url = 'mongodb+srv://srkvodnala:srkvshiva87@cluster0-swgxv.mongodb.net/test?retryWrites=true&w=majority'
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+const dev_db_url = process.env.ATLAS_URI;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
@@ -31,6 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
